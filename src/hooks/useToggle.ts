f98d7501dd88a28ab5c98da5b40ad7464a581ac6
bya2 <byaa1972@gmail.com>
2022-11-21 21:52:05 +0900

    Add return type of hook

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useToggle = (_value: boolean) => {
+const useToggle = (_value: boolean): [boolean, () => void] => {
   const [state, set] = useState(_value);
 
   const turn = () => set((prev) => !prev);
